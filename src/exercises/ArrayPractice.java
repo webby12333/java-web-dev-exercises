a1ee3f38fc79462ef6e17e52ee5711155a79991f
webby12333 <webby12333@yahoo.com>
2020-04-27 19:00:03 -0500

    Array Practice

--- src/exercises/ArrayPractice.java
+++ src/exercises/ArrayPractice.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class ArrayPractice {
     public static void main(String[] args) {
+        String test = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
         int values[] = {1, 1, 2, 3, 5, 8};
         for (int i : values) {
             if (i % 2 != 0) {
