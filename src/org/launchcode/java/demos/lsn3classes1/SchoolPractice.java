90aaaebfffb5f986829c9b776c2a6dde2b0be774
webby12333 <webby12333@yahoo.com>
2020-06-04 20:11:57 -0500

    "Finished 5"

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -3,7 +3,7 @@ package org.launchcode.java.demos.lsn3classes1;
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
-        Student michael = new Student();
+        Student michael = new Student("michael", 4500);
         michael.setName("Michael");
         michael.setId(4500);
         michael.setCredits(1);
--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -5,10 +5,56 @@ package org.launchcode.java.demos.lsn3classes1;
 
 public class Student {
 
+    private static int nextStudentId = 1;
     private String name;
     private int studentId;
-    private int numberOfCredits = 0;
-    private double gpa = 0.0;
+    private int numberOfCredits;
+    private double gpa;
+
+    public Student(String name, int studentId, int numberOfCredits, double gpa) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+    public Student(String name, int studentId) {
+        this(name, studentId, 0, 0);
+    }
+
+    public Student(String name) {
+        this(name, nextStudentId);
+        nextStudentId++;
+    }
+
+    public String studentInfo() {
+        return (this.name + " has a GPA of: " + this.gpa);
+    }
+
+    public void addGrade(int courseCredits, double grade) {
+        double currentQualityScore = gpa * numberOfCredits;
+        double classScore = grade * courseCredits;
+        double newQualityScore = currentQualityScore + classScore;
+        numberOfCredits += courseCredits;
+        gpa = newQualityScore / numberOfCredits;
+    }
+
+    public String getGradeLevel() {
+        String gradeLevel = "";
+        if (numberOfCredits < 29) {
+            gradeLevel = "Freshman";
+        } else if (numberOfCredits < 59) {
+            gradeLevel = "Sophomore";
+        } else if (numberOfCredits < 89) {
+            gradeLevel = "Junior";
+        } else if (numberOfCredits > 90) {
+            gradeLevel = "Senior";
+        }
+        return gradeLevel;
+    }
+
+    /* getters and setters omitted */
+
+
 
     public String getName() {
         return name;
